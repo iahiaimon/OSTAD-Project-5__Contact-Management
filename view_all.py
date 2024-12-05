c59960b7af211ad5048fa00db87efe5ada3091e8
iahia <iahiaimon555@gmail.com>
2024-12-05 15:58:56 +0600

    Uploading

--- view_all.py
+++ view_all.py
@@ -3,7 +3,7 @@ from save_all_contact import store
 def view_all(all_contacts):
     if all_contacts != []:
         for contact in all_contacts:
-            print(f"\n{contact['number']} , {contact['name']} , {contact['email']} , {contact['address']} \n") 
+            print(f"\n{contact['number']} , {contact['name']} , {contact['email']} , {contact['address']}") 
         
     else:
         print("\nYour contact list is empty")
